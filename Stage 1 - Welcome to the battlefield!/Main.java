b11782f5800e761684c1a5386b57020f121f3dfb
Combat47 <115223808+combat47@users.noreply.github.com>
2023-09-27 10:34:34 +0330

    Create Main.java

---
+++ Stage 1 - Welcome to the battlefield!/Main.java
@@ -0,0 +1,12 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("""
+            X O X
+            O X O
+            X X O 
+                           """);
+    }
+}
